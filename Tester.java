6eb0494afe9be7046fe94155a1349c81bd0296e4
Naman Pandey <naman.2406677101@vcet.edu.in>
2025-10-03 11:26:22 +0530

    Add files via upload
    Added all assignments

---
+++ Tester.java
@@ -0,0 +1,105 @@
+package Stack.Assignment1;
+
+class Stack {
+    
+    private int top; 
+    private int maxSize; 
+    private int[] arr;
+
+    Stack(int maxSize) {
+        this.top = -1; 
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+    }
+
+    public boolean isFull() {
+        if (top >= (maxSize - 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean push(int data) {
+        if (isFull()) {
+            return false;
+        }
+        else {
+            arr[++top] = data;
+            return true;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty())
+            return Integer.MIN_VALUE;
+        else
+            return arr[top];
+    }
+
+    public void display() {
+        if (isEmpty())
+            System.out.println("Stack is empty!");
+        else {
+            System.out.println("Displaying stack elements");
+            for (int index = top; index >= 0; index--) {
+                System.out.println(arr[index]); // accessing element at position index
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int pop() {
+        if (isEmpty())
+            return Integer.MIN_VALUE;
+        else
+            return arr[top--];
+    }
+}
+
+
+class Tester {
+      
+    public static void main(String args[]) {
+            
+        Stack stack = new Stack(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        
+        calculateSum(stack);
+            
+        System.out.println("Updated stack");
+        stack.display();
+    }
+
+    public static void calculateSum(Stack stack) {
+        int sum = 0;
+        Stack tempStack = new Stack(10); // Assuming a max size for the temp stack
+
+        // Pop elements, calculate sum, and store in a temporary stack
+        while (!stack.isEmpty()) {
+            int element = stack.pop();
+            sum += element;
+            tempStack.push(element);
+        }
+
+        // Push the sum to the bottom of the original stack
+        stack.push(sum);
+
+        // Push the elements back from the temporary stack to the original stack
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+    }
+}
+
+
+
+
