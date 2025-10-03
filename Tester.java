55bb075ecc2aeb97da35f494bcc30254057584fe
Naman Pandey <naman.2406677101@vcet.edu.in>
2025-10-03 11:28:02 +0530

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,105 +1,43 @@
-package Stack.Assignment1;
-
-class Stack {
-    
-    private int top; 
-    private int maxSize; 
-    private int[] arr;
-
-    Stack(int maxSize) {
-        this.top = -1; 
-        this.maxSize = maxSize;
-        arr = new int[maxSize];
-    }
-
-    public boolean isFull() {
-        if (top >= (maxSize - 1)) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean push(int data) {
-        if (isFull()) {
-            return false;
-        }
-        else {
-            arr[++top] = data;
-            return true;
-        }
-    }
-
-    public int peek() {
-        if (isEmpty())
-            return Integer.MIN_VALUE;
-        else
-            return arr[top];
-    }
-
-    public void display() {
-        if (isEmpty())
-            System.out.println("Stack is empty!");
-        else {
-            System.out.println("Displaying stack elements");
-            for (int index = top; index >= 0; index--) {
-                System.out.println(arr[index]); // accessing element at position index
-            }
-        }
-    }
-
-    public boolean isEmpty() {
-        if (top < 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public int pop() {
-        if (isEmpty())
-            return Integer.MIN_VALUE;
-        else
-            return arr[top--];
-    }
-}
-
+package BinarySearch.Assignment1;
 
 class Tester {
-      
-    public static void main(String args[]) {
-            
-        Stack stack = new Stack(10);
-        stack.push(15);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        
-        calculateSum(stack);
-            
-        System.out.println("Updated stack");
-        stack.display();
-    }
 
-    public static void calculateSum(Stack stack) {
-        int sum = 0;
-        Stack tempStack = new Stack(10); // Assuming a max size for the temp stack
+    public static int searchCustomerId(int customerIds[], int customerIdToBeSearched) {
+        int low = 0;
+        int high = customerIds.length - 1;
 
-        // Pop elements, calculate sum, and store in a temporary stack
-        while (!stack.isEmpty()) {
-            int element = stack.pop();
-            sum += element;
-            tempStack.push(element);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if customerIdToBeSearched is present at mid
+            if (customerIds[mid] == customerIdToBeSearched) {
+                return mid;
+            }
+
+            // If customerIdToBeSearched is greater, ignore the left half
+            if (customerIds[mid] < customerIdToBeSearched) {
+                low = mid + 1;
+            } 
+            // If customerIdToBeSearched is smaller, ignore the right half
+            else {
+                high = mid - 1;
+            }
         }
 
-        // Push the sum to the bottom of the original stack
-        stack.push(sum);
-
-        // Push the elements back from the temporary stack to the original stack
-        while (!tempStack.isEmpty()) {
-            stack.push(tempStack.pop());
-        }
+        // If we reach here, then the element was not present
+        return -1;
     }
+    
+    public static void main(String[] args) {
+		int[] customerIds = { 80451, 80462, 80465, 80479, 80550, 80561, 80665, 80770 };
+		int customerIdToBeSearched = 80462;
+
+		int index = searchCustomerId(customerIds, customerIdToBeSearched);
+		
+		if (index == -1)
+			System.out.println("Customer Id " + customerIdToBeSearched + " is not found!");
+		else
+			System.out.println("Customer Id " + customerIdToBeSearched + " is found at index position " + index+"!");
+	}
+
 }
-
-
-
-
